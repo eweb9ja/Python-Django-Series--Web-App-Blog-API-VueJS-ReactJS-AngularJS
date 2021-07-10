d1aaeac5d97d90c7e316e7766513e92bd5069cc8
eweb9ja <ekesygroup@gmail.com>
2021-07-10 15:32:34 -0700

    Add URLs to APP

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
+    path('about-us/', include('about.urls')),
+    path('blog/', include('home.urls')),
+    path('service-and-product', include('service_product.urls')),
+    path('contact-us', include('contact_us.urls')),
+    
     path('admin/', admin.site.urls),
 ]
