6425e3237630e47d587e1492a414fbb885cc0f77
eweb9ja <ekesygroup@gmail.com>
2021-07-11 02:48:22 -0700

    Add Model to Blog page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import admin
 
-# Create your models here.
+class BlogPage(models.Model):
+    title = models.CharField(max_length=500, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='blog_img/%Y/%m/%d/', null=True, blank=True)
+    updated = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class BlogPageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'updated', 'created_date')
